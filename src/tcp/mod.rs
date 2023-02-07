476cfd5cc0d3e8f366bb7b70af87b1830bb90eda
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2023-02-07 13:09:59 +0000

    Update mod.rs

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -611,7 +611,7 @@ impl Stack {
                     };
                     tuples.remove(&tuple);
                     for i in tuples.iter() {
-                        trace!("tuple: {:?}", i)
+                        trace!("tuple: {:?}", i.AddrTuple)
                     }
                 },
                 size = tun.recv(&mut recv_buf[..]) => {
