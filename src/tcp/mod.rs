5f48754583b43d92ece8f83d818dab680a0fc96d
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2023-02-13 13:03:27 +0000

    Update mod.rs

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -82,8 +82,8 @@ impl<'a> std::iter::IntoIterator for &'a AddrTuple {
     type IntoIter = std::slice::Iter<'a, SocketAddr>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.local_addr();
-        self.remote_addr();
+        self.local_addr;
+        self.remote_addr;
     }
 }
 
