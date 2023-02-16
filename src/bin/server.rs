a2c4482d44c4f94c19d603d9c08d99c66321eeea
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2023-02-16 06:23:07 +0000

    Update server.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -449,6 +449,7 @@ async fn main_async(matches: ArgMatches) -> io::Result<()> {
                 SocketAddr::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0)), 0)
             })
             .await;
+            debug!("0{first_port}");
 
             let udp_sock = match udp_sock {
                 Ok(udp_sock) => udp_sock,
@@ -479,6 +480,7 @@ async fn main_async(matches: ArgMatches) -> io::Result<()> {
                 }
                 socks
             };
+            debug!("1{first_port}");
 
             let udp_socks = Arc::new(udp_socks);
 
