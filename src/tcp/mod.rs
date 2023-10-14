256d4b29567c2dc2cc7f4a26de1226661e518bd8
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2023-10-14 17:12:41 +0000

    Update mod.rs

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -289,6 +289,7 @@ impl Socket {
                                         seq_sent = true;
                                         continue;
                                     }
+                                    error!("channel {} recv error: {err}", self);
                                 }
 
                             }
